7ebe1f859f6c56d127c85f020f012a6e0a87c993
EDawn072004 <69882381+EDawn072004@users.noreply.github.com>
2020-11-30 09:00:34 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+txrx = 0 # 0 is receiver; 1 is transmitter
+
 radio.set_group(1)
+acceleration = input.acceleration(Dimension.X)
+
+if input.button_is_pressed(Button.A):
+    txrx = 0
+
+if input.button_is_pressed(Button.B):
+    txrx = 1
 
 def on_forever():
-    acceleration = input.acceleration(Dimension.X)
-    radio.send_number(acceleration)
+    if txrx == 1:
+        radio.send_number(acceleration)
 basic.forever(on_forever)
 
+"""
 def on_received_number(receivedNumber):
-    basic.show_number(receivedNumber)
-    print(receivedNumber)
+    led.toggle(0, 0)
+    basic.show_string("Receiver")
     serial.write_value("x", receivedNumber)
 radio.on_received_number(on_received_number)
+"""
--- main.ts
+++ main.ts
@@ -1,10 +1,26 @@
+let txrx = 0
+//  0 is receiver; 1 is transmitter
 radio.setGroup(1)
+let acceleration = input.acceleration(Dimension.X)
+if (input.buttonIsPressed(Button.A)) {
+    txrx = 0
+}
+
+if (input.buttonIsPressed(Button.B)) {
+    txrx = 1
+}
+
 basic.forever(function on_forever() {
-    let acceleration = input.acceleration(Dimension.X)
-    radio.sendNumber(acceleration)
+    if (txrx == 1) {
+        radio.sendNumber(acceleration)
+    }
+    
 })
-radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
-    basic.showNumber(receivedNumber)
-    console.log(receivedNumber)
-    serial.writeValue("x", receivedNumber)
-})
+/** 
+def on_received_number(receivedNumber):
+    led.toggle(0, 0)
+    basic.show_string("Receiver")
+    serial.write_value("x", receivedNumber)
+radio.on_received_number(on_received_number)
+
+ */
