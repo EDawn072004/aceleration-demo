23a0cc874475d5eed8f15d1ea8fd5bc22a507e7f
EDawn072004 <69882381+EDawn072004@users.noreply.github.com>
2020-12-01 08:44:22 -0500

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-txrx = 0 # 0 is receiver; 1 is transmitter
-
+acceleration = 0
+txrx = 0
+# 0 is receiver; 1 is transmitter
 radio.set_group(1)
-acceleration = input.acceleration(Dimension.X)
+serial.write_line("hfjgkj")
+"""
+
+def on_received_number(receivedNumber):
+
+basic.show_string("Receiver")
+
+serial.write_value("x", receivedNumber)
+
+radio.on_received_number(on_received_number)
+
+"""
 
 def on_forever():
+    global acceleration, txrx
+    txrx2 = 0
+    acceleration = input.acceleration(Dimension.X)
     led.toggle(0, 0)
     if input.button_is_pressed(Button.A):
         txrx = 0
@@ -13,14 +28,6 @@ def on_forever():
         basic.show_string("T")
     if txrx == 1:
         radio.send_number(acceleration)
-    if txrx == 0:
-        serial.write_value("x", acceleration)
+    if txrx2 == 0:
+        pass
 basic.forever(on_forever)
-
-"""
-def on_received_number(receivedNumber):
-    
-    basic.show_string("Receiver")
-    serial.write_value("x", receivedNumber)
-radio.on_received_number(on_received_number)
-"""
--- main.ts
+++ main.ts
@@ -1,34 +1,32 @@
+let acceleration = 0
 let txrx = 0
-//  0 is receiver; 1 is transmitter
+// 0 is receiver; 1 is transmitter
 radio.setGroup(1)
-let acceleration = input.acceleration(Dimension.X)
-basic.forever(function on_forever() {
-    let txrx: number;
+serial.writeLine("hfjgkj")
+/**
+ * def on_received_number(receivedNumber):
+ * 
+ * basic.show_string("Receiver")
+ * 
+ * serial.write_value("x", receivedNumber)
+ * 
+ * radio.on_received_number(on_received_number)
+ */
+basic.forever(function () {
+    acceleration = input.acceleration(Dimension.X)
     led.toggle(0, 0)
     if (input.buttonIsPressed(Button.A)) {
         txrx = 0
         basic.showString("R")
     }
-    
     if (input.buttonIsPressed(Button.B)) {
         txrx = 1
         basic.showString("T")
     }
-    
     if (txrx == 1) {
         radio.sendNumber(acceleration)
     }
-    
     if (txrx == 0) {
-        serial.writeValue("x", acceleration)
+        serial.writeNumber(acceleration)
     }
-    
 })
-/** 
-def on_received_number(receivedNumber):
-    
-    basic.show_string("Receiver")
-    serial.write_value("x", receivedNumber)
-radio.on_received_number(on_received_number)
-
- */
